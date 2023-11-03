9b53e590becb468c50e0c1727e380ff90739df6d
Ryan <racollette@gmail.com>
2023-11-02 21:20:51 -0400

    nft hover details

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { useBlockNumber } from "wagmi";
+import { MintNFT } from "~/components/Mint";
+import { useNetwork } from "wagmi"; // Import the useNetwork hook
 
 const images = [
   "/NFT1_AuXPoW.png",
@@ -24,23 +26,34 @@ const finalXTranslation = {
 };
 
 const nftType = {
-  1: "Proof of Work",
-  2: "Proof of Stake",
-  3: "Proof of Integrity",
+  1: { title: "Proof of Work", details: "Earn $GLIDE" },
+  2: {
+    title: "Proof of Stake",
+    details: "50% discount on Elasafe (Elastos node service provider)",
+  },
+  3: {
+    title: "Proof of Integrity",
+    details: "20% discount using Elacity Flint’s generative AI tools",
+  },
 };
 
+function getNFTNumber(url: string): 1 | 2 | 3 {
+  if (url.includes("PoW")) return 1;
+  if (url.includes("BPoS")) return 2;
+  return 3;
+}
+
 const Spinner: React.FC = () => {
+  const { chain } = useNetwork();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [spin, setSpin] = useState(false);
   const [spinState, setSpinState] = useState<1 | 2 | 3>(1);
   const [type, setType] = useState<string>("");
-  const [startingPoint, setStartingPoint] = useState();
   const spinnerRef = useRef<HTMLDivElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const spinDuration = 10000;
 
   const { data, isError, isLoading } = useBlockNumber();
-  console.log(data);
 
   const handleSpin = () => {
     // spinnerRef.current?.style.setProperty("transform", `translateX(-100px)`);
@@ -55,12 +68,12 @@ const Spinner: React.FC = () => {
 
     setTimeout(() => {
       setSpin(false);
-      setType(nftType[randomNumber]);
+      setType(nftType[randomNumber].title);
     }, spinDuration);
   };
 
-  console.log(spinState);
-  console.log(type);
+  //console.log(spinState);
+  //console.log(type);
 
   useEffect(() => {
     const spinnerWidth = spinnerRef.current?.clientWidth;
@@ -73,18 +86,22 @@ const Spinner: React.FC = () => {
     // console.log(imagesFromStart);
   }, [spin]);
 
+  console.log(spinnerImages);
+
   return (
     <div
       className="flex h-[500px] w-4/5 flex-col items-center gap-8 overflow-hidden"
       ref={containerRef}
     >
-      <button
-        className="text-md h-20 w-1/4 rounded-lg bg-fuchsia-600 font-bold text-white hover:bg-fuchsia-500 disabled:cursor-not-allowed"
-        onClick={handleSpin}
-        disabled={spin}
-      >
-        Spin
-      </button>
+      <div className="w-full">
+        <MintNFT
+          handleSpin={handleSpin}
+          spinning={spin}
+          connected={(chain && chain.id === 20) ?? false}
+          spinState={spinState}
+        />
+      </div>
+
       <div className="relative flex h-full w-full items-center justify-center overflow-hidden">
         <div
           ref={spinnerRef}
@@ -100,16 +117,26 @@ const Spinner: React.FC = () => {
           //   style={{ animation: "carousel-spin-1 10 linear infinite" }}
         >
           {spinnerImages.map((image, index) => (
-            <Image
-              key={index}
-              src={image}
-              alt={`Image ${index}`}
-              height={316}
-              width={316}
-              className={`mx-0.5 transition-opacity ${
-                index === currentIndex ? "opacity-100" : "opacity-100"
-              }`}
-            />
+            <div key={index} className="group relative h-[316px] w-[316px]">
+              <Image
+                src={image}
+                alt={`Image ${index}`}
+                width={316}
+                height={316}
+                className={`mx-0.5 transition-opacity ${
+                  index === currentIndex ? "opacity-100" : "opacity-100"
+                }`}
+              />
+              <div className="absolute left-0 top-0 flex h-full w-full transform flex-col items-center justify-center gap-2 bg-black bg-opacity-80 text-white opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+                <h1 className="text-xl font-extrabold">
+                  {nftType[getNFTNumber(image)].title}
+                </h1>
+                <p className="w-5/6 text-center">
+                  <span className="font-bold">Utility:&nbsp;</span>
+                  {nftType[getNFTNumber(image)].details}
+                </p>
+              </div>
+            </div>
           ))}
         </div>
         <div className="absolute left-1/2 -translate-x-1/2 transform text-center">
