4427ea65b00b0a45449f5da062823c2c3c9e246b
racollette <racollette@gmail.com>
2023-11-13 15:13:23 -0500

    add dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useAccount, useContractRead } from "wagmi";
+import { Header } from "~/components/Header";
+import { abi } from "~/utils/contracts";
+import dynamic from "next/dynamic";
+
+const CompositeHoldingsNoSSR = dynamic(
+  () => import("~/components/CompositeHoldings"),
+  { ssr: false },
+);
+
+const RewardsNoSSR = dynamic(() => import("~/components/Rewards"), {
+  ssr: false,
+});
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>Elastic Consensus NFT | Dashboard</title>
+        <meta name="description" content="Celebrating Elastic Consensus" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nosifer&family=Shadows+Into+Light&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header />
+      <main className="relative flex min-h-screen flex-col gap-20 bg-gradient-to-b from-[#2e026d] to-[#15162c] pt-12 text-white">
+        <CompositeHoldingsNoSSR />
+        <RewardsNoSSR />
+      </main>
+    </>
+  );
+}
