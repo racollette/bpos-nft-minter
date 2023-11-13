0a65f2e9305dd0e4a114991b0579ba941ef68b44
racollette <racollette@gmail.com>
2023-11-13 15:12:40 -0500

    create rewards component

---
+++ src/components/Rewards.tsx
@@ -0,0 +1,14 @@
+const Rewards = () => {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4">
+      <div className="text-center font-shadows text-3xl font-extrabold text-white">
+        Rewards
+      </div>
+      <p>Rewards will be available here starting 11/17</p>
+      <div>BPoS NFT - Earn GLIDE</div>
+      <div>All 3 - Earn ELA</div>
+    </section>
+  );
+};
+
+export default Rewards;
