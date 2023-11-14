dae7c48c3cf236bd72edd01b0009da7217ae4ac4
racollette <racollette@gmail.com>
2023-11-13 22:08:50 -0500

    restyle header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
+import Profile from "./Profile";
 
 export const Header = () => {
   return (
-    <div className="z-10 flex flex-row items-center justify-end gap-6 bg-[#2e026d] px-6 py-1">
-      <Link className="font-shadows text-lg font-bold text-white" href="/">
-        Mint
-      </Link>
-      <Link
-        className="font-shadows text-lg font-bold text-white"
-        href="/dashboard"
-      >
-        Dashboard
-      </Link>
+    <div className="z-10 flex flex-row items-center justify-between bg-indigo-950 px-6 py-1">
+      <div className="flex flex-row items-center gap-6 self-center">
+        <Link className="font-shadows text-lg font-bold text-white" href="/">
+          Mint
+        </Link>
+        <Link
+          className="font-shadows text-lg font-bold text-white"
+          href="/dashboard"
+        >
+          Dashboard
+        </Link>
+      </div>
+      <div className="justify-self-end">
+        <Profile size="sm" />
+      </div>
     </div>
   );
 };
